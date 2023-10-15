711804b78555043c125ab378a87c0411033763eb
Ashish Kumar <kashish979@gmail.com>
2023-10-15 21:08:19 +0530

    Added some Basic Commands

---
+++ Basics Python/Data_Types.py
@@ -0,0 +1,30 @@
+#Interger
+my_int = 432
+
+
+#Float
+my_float = 12.45
+
+
+#String
+my_string = "Hello Python !"
+
+#Boolean
+is_true = True
+
+#List
+my_list = [1, 2, 3.14, "Hello Python"]
+
+#Tuple
+my_tuple = (5, 6, 2, 'rede')
+
+#Set
+my_set = (1, 4, 6, 7)
+
+#Dict
+my_dict = {'f_name' : 'John', 'l_name' : 'wick', 'age' : 12 }
+
+my_none = None
+
+print("Hello World")
+print(f"Interger {my_int} {type(my_int)}")  
